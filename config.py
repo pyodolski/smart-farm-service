c57613f5a73c9f91a34b2f0884c06c96b5c96330
pocketopii <rlwns00@gmail.com>
2025-08-31 00:40:27 +0900

    v3

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ DB_CONFIG = {
     'user': os.getenv('DB_USER', 'root'),
     'password': os.getenv('DB_PASSWORD', '1234'),
     'database': os.getenv('DB_NAME', 'smartfarm'),
+    'port': int(os.getenv('DB_PORT', 5432)),  # PostgreSQL 기본 포트
     'charset': 'utf8mb4'
 }
